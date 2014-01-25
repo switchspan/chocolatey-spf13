d8495e47965773c667c92e55e2f30fac661acc38
switchspan <taylor.kenneth@gmail.com>
2014-01-25 13:55:47 -0500

    updated the install script to call the spf13 command file

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -9,22 +9,24 @@ $validExitCodes = @(0) #please insert other valid exit codes here, exit codes fo
 # main helpers - these have error handling tucked into them already
 # installer, will assert administrative rights
 # if removing $url64, please remove from here
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+#Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
 # download and unpack a zip file
 # if removing $url64, please remove from here
 #Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
+try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
   # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
   # downloader that the main helpers use to download items
   # if removing $url64, please remove from here
-  #Get-ChocolateyWebFile "$packageName" 'DOWNLOAD_TO_FILE_FULL_PATH' "$url" "$url64"
+  $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $cmdPath = Join-Path $scriptPath 'spf13-vim-windows-install.cmd'
+  Get-ChocolateyWebFile "$packageName" "$cmdPath" "$url"
   # installer, will assert administrative rights - used by Install-ChocolateyPackage
   #Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" '_FULLFILEPATH_' -validExitCodes $validExitCodes
   # unzips a file to the specified location - auto overwrites existing content
   #Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   # Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
-  #Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
+  Start-ChocolateyProcessAsAdmin "$silentArgs" $cmdPath -validExitCodes $validExitCodes
   # add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
   #Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
   # add specific files as shortcuts to the desktop
@@ -39,8 +41,8 @@ Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -
 
 
   # the following is all part of error handling
-  #Write-ChocolateySuccess "$packageName"
-#} catch {
-  #Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  #throw
-#}
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
