2b650de5e2d9b365a81d3d784c2a50e88fdc88b0
switchspan <taylor.kenneth@gmail.com>
2014-01-25 16:26:24 -0500

    renamed nuspec file and created packages

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$packageName = 'spf13'
+﻿$packageName = 'spf13.vim'
 $installerType = 'EXE'
 $vimUrl = 'ftp://ftp.vim.org/pub/vim/pc/gvim74.exe'
 $spfUrl = 'https://github.com/spf13/spf13-vim/raw/3.0/spf13-vim-windows-install.cmd'
