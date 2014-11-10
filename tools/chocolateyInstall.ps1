b8267248057de9fab711090528aa36ac13b2bf45
switchspan <taylor.kenneth@gmail.com>
2014-11-09 23:14:52 -0500

    Change to powershell only

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,6 @@
 ﻿$packageName = 'spf13-vim'
 $spfUrl = 'https://github.com/spf13/spf13-vim/raw/3.0/spf13-vim-windows-install.cmd'
 $silentArgs = '/S' 
-$exeToRun = 'cmd.exe'
 $validExitCodes = @(0,1) 
 
 # Download and install spf13
@@ -10,8 +9,7 @@ try {
   $cmdPath = Join-Path $scriptPath 'spf13-vim-windows-install.cmd'
 
   Get-ChocolateyWebFile "$packageName" "$cmdPath" "$spfUrl"
-  #Start-ChocolateyProcessAsAdmin $cmdPath $exeToRun -validExitCodes $validExitCodes
-  Start-ChocolateyProcessAsAdmin "/c $cmdPath" $exeToRun
+  Start-ChocolateyProcessAsAdmin $cmdPath -validExitCodes $validExitCodes
   Write-ChocolateySuccess "$packageName"
 } catch {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
