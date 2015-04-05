3b8749d4400fe5e065ee7f156861fb78b84a2090
John Fisher <juanpescador@users.noreply.github.com>
2015-04-05 03:24:12 +0200

    Delete symlinks during uninstallation.
    Checks they're actually symlinks before deleting, incase the user is
    no longer using spf13-vim and has replaced the symlinks with actual
    files, e.g. their own .vimrc or .vim directory. Also treats directory
    symlinks appropriately so as to not delete the contents of the symlink.

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -2,9 +2,44 @@
 $packageName = 'spf13-vim'
 $installDir = Join-Path $HOME '.spf13-vim-3'
 
-try {
-  Remove-Item -Recurse -Force $installDir
+# Test whether path is a symlink, from http://stackoverflow.com/a/818054/
+function Test-ReparsePoint([string]$path) {
+  $file = Get-Item $path -Force -ea 0
+  return [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
+}
 
+function Remove-SymLink([string]$path)
+{
+    # Only remove if it's a symlink.
+    If (Test-ReparsePoint $path)
+    {
+        If (Test-Path -PathType Container $path)
+        {
+            # rmdir safely deletes the directory symlink, without deleting its
+            # contents (http://superuser.com/a/306618/)
+            Invoke-Expression "cmd /c rmdir $path"
+        }
+        Else
+        {
+            Invoke-Expression "cmd /c del $path"
+        }
+    }
+}
+
+try {
+  Write-Host "Deleting symbolic links"
+  # Delete the symbolic links
+  Remove-Symlink "$HOME\.vimrc"
+  Remove-Symlink "$HOME\_vimrc"
+  Remove-Symlink "$HOME\.vimrc.fork"
+  Remove-Symlink "$HOME\.vimrc.bundles"
+  Remove-Symlink "$HOME\.vimrc.bundles.fork"
+  Remove-Symlink "$HOME\.vimrc.before"
+  Remove-Symlink "$HOME\.vimrc.before.fork"
+  Remove-Symlink "$HOME\.vim"
+
+  Remove-Item -Recurse -Force $installDir
+  
   Write-ChocolateySuccess "$packageName"
 } catch {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
